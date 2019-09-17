c5c76b48e0b74d9f6209e21e1956467a8866155f
Yanis Guaye <yguaye44@gmail.com>
2019-09-17 13:07:55 +0200

    Update to mc 19w37a

--- src/main/java/com/misterpemodder/shulkerboxtooltip/impl/ModMenuCompat.java
+++ src/main/java/com/misterpemodder/shulkerboxtooltip/impl/ModMenuCompat.java
@@ -1,10 +1,9 @@
 package com.misterpemodder.shulkerboxtooltip.impl;
 
-import java.util.Optional;
-import java.util.function.Supplier;
-import com.misterpemodder.shulkerboxtooltip.impl.config.Configuration;
+import java.util.function.Function;
+// import com.misterpemodder.shulkerboxtooltip.impl.config.Configuration;
 import io.github.prospector.modmenu.api.ModMenuApi;
-import me.sargunvohra.mcmods.autoconfig1.AutoConfig;
+// import me.sargunvohra.mcmods.autoconfig1.AutoConfig;
 import net.minecraft.client.gui.screen.Screen;
 
 public class ModMenuCompat implements ModMenuApi {
@@ -14,7 +13,9 @@ public class ModMenuCompat implements ModMenuApi {
   }
 
   @Override
-  public Optional<Supplier<Screen>> getConfigScreen(Screen screen) {
-    return Optional.of(AutoConfig.getConfigScreen(Configuration.class, screen));
+  public Function<Screen, ? extends Screen> getConfigScreenFactory() {
+    return screen -> null;
+    // Creating a config screen crashes the game in the current version of Auto Config.
+    // return screen -> AutoConfig.getConfigScreen(Configuration.class, screen).get();
   }
 }
