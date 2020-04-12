5633bab6444d7e79c08bb4ffc8bb945469390817
Yanis Guaye <yguaye44@gmail.com>
2020-04-12 19:33:39 +0200

    Fix the preview window being drawn before its position is updated in the creative search menu

--- src/main/java/com/misterpemodder/shulkerboxtooltip/mixin/client/CreativeInventoryScreenMixin.java
+++ src/main/java/com/misterpemodder/shulkerboxtooltip/mixin/client/CreativeInventoryScreenMixin.java
@@ -5,6 +5,7 @@ import com.misterpemodder.shulkerboxtooltip.impl.ShulkerBoxTooltipClient;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.At.Shift;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import net.minecraft.client.gui.screen.Screen;
 import net.minecraft.client.gui.screen.ingame.CreativeInventoryScreen;
@@ -12,8 +13,11 @@ import net.minecraft.item.ItemStack;
 
 @Mixin(CreativeInventoryScreen.class)
 public class CreativeInventoryScreenMixin {
-  @Inject(at = @At(value = "INVOKE",
-      target = "net/minecraft/client/gui/screen/ingame/CreativeInventoryScreen.renderTooltip(Ljava/util/List;II)V"),
+  @Inject(
+      at = @At(value = "INVOKE",
+          target = "net/minecraft/client/gui/screen/ingame/CreativeInventoryScreen.renderTooltip"
+              + "(Ljava/util/List;II)V",
+          shift = Shift.AFTER),
       method = "Lnet/minecraft/client/gui/screen/ingame/CreativeInventoryScreen;renderTooltip"
           + "(Lnet/minecraft/item/ItemStack;II)V")
   private void onDrawMousehoverTooltip(ItemStack stack, int mouseX, int mouseY, CallbackInfo ci) {
