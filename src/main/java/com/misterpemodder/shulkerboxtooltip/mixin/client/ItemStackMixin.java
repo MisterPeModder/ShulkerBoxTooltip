76ac91f97231a343dfc16e60158e2c1cd3143566
Yanis Guaye <yguaye44@gmail.com>
2020-04-22 23:56:14 +0200

    NONE tooltip type now hides item lore Fixes #16

--- src/main/java/com/misterpemodder/shulkerboxtooltip/mixin/client/ItemStackMixin.java
+++ src/main/java/com/misterpemodder/shulkerboxtooltip/mixin/client/ItemStackMixin.java
@@ -1,23 +1,50 @@
 package com.misterpemodder.shulkerboxtooltip.mixin.client;
 
 import java.util.List;
+import javax.annotation.Nullable;
+import com.misterpemodder.shulkerboxtooltip.impl.ShulkerBoxTooltip;
 import com.misterpemodder.shulkerboxtooltip.impl.ShulkerBoxTooltipClient;
+import com.misterpemodder.shulkerboxtooltip.impl.config.Configuration.ShulkerBoxTooltipType;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.Redirect;
+import org.spongepowered.asm.mixin.injection.Slice;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
-import org.spongepowered.asm.mixin.injection.callback.LocalCapture;
+import net.minecraft.block.ShulkerBoxBlock;
 import net.minecraft.client.item.TooltipContext;
 import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.item.BlockItem;
+import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
+import net.minecraft.nbt.CompoundTag;
 import net.minecraft.text.Text;
 
 @Mixin(ItemStack.class)
 public class ItemStackMixin {
-  @Inject(at = @At("RETURN"), locals = LocalCapture.CAPTURE_FAILSOFT,
-      method = "Lnet/minecraft/item/ItemStack;getTooltip(Lnet/minecraft/entity/player/PlayerEntity;Lnet/minecraft/client/item/TooltipContext;)Ljava/util/List;")
+  @Inject(at = @At("RETURN"),
+      method = "Lnet/minecraft/item/ItemStack;getTooltip"
+          + "(Lnet/minecraft/entity/player/PlayerEntity;Lnet/minecraft/client/item/TooltipContext;)"
+          + "Ljava/util/List;")
   private void onGetTooltip(PlayerEntity player, TooltipContext context,
-      CallbackInfoReturnable<List<Text>> ci, List<Text> tooltip) {
-    ShulkerBoxTooltipClient.modifyStackTooltip((ItemStack) (Object) this, tooltip);
+      CallbackInfoReturnable<List<Text>> ci) {
+    ShulkerBoxTooltipClient.modifyStackTooltip((ItemStack) (Object) this, ci.getReturnValue());
+  }
+
+  @Redirect(
+      at = @At(value = "INVOKE",
+          target = "Lnet/minecraft/nbt/CompoundTag;contains(Ljava/lang/String;I)Z", ordinal = 0),
+      method = "Lnet/minecraft/item/ItemStack;getTooltip"
+          + "(Lnet/minecraft/entity/player/PlayerEntity;Lnet/minecraft/client/item/TooltipContext;)"
+          + "Ljava/util/List;",
+      slice = @Slice(from = @At(value = "CONSTANT", args = "stringValue=display", ordinal = 0)))
+  private boolean removeLoreIfDisabled(CompoundTag tag, String key, int type,
+      @Nullable PlayerEntity player, TooltipContext context) {
+    Item item = ((ItemStack) (Object) this).getItem();
+
+    if (ShulkerBoxTooltip.config.main.tooltipType == ShulkerBoxTooltipType.NONE
+        && item instanceof BlockItem && ((BlockItem) item).getBlock() instanceof ShulkerBoxBlock)
+      return false;
+    return tag.contains(key, type);
   }
 }
--- src/main/java/com/misterpemodder/shulkerboxtooltip/mixin/client/ShulkerBoxBlockMixin.java
+++ src/main/java/com/misterpemodder/shulkerboxtooltip/mixin/client/ShulkerBoxBlockMixin.java
@@ -8,12 +8,10 @@ import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
-import org.spongepowered.asm.mixin.injection.callback.LocalCapture;
 import net.minecraft.block.BlockWithEntity;
 import net.minecraft.block.ShulkerBoxBlock;
 import net.minecraft.client.item.TooltipContext;
 import net.minecraft.item.ItemStack;
-import net.minecraft.nbt.CompoundTag;
 import net.minecraft.text.Text;
 import net.minecraft.world.BlockView;
 
@@ -30,9 +28,9 @@ public abstract class ShulkerBoxBlockMixin extends BlockWithEntity {
       method = "Lnet/minecraft/block/ShulkerBoxBlock;buildTooltip(Lnet/minecraft/item/ItemStack;"
           + "Lnet/minecraft/world/BlockView;Ljava/util/List;"
           + "Lnet/minecraft/client/item/TooltipContext;)V",
-      cancellable = true, locals = LocalCapture.CAPTURE_FAILSOFT)
+      cancellable = true)
   private void onBuildTooltip(ItemStack stack, @Nullable BlockView view, List<Text> tooltip,
-      TooltipContext options, CallbackInfo ci, CompoundTag compound) {
+      TooltipContext options, CallbackInfo ci) {
     if (ShulkerBoxTooltip.config.main.tooltipType != ShulkerBoxTooltipType.VANILLA)
       ci.cancel();
   }
