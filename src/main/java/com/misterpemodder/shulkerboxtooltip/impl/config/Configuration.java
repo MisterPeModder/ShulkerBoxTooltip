20731c9058dbd7bbd3d02aa9f7701ad58fabcc10
Yanis Guaye <yguaye44@gmail.com>
2020-06-06 18:09:17 +0200

    Add comments in config file

--- src/main/java/com/misterpemodder/shulkerboxtooltip/impl/config/Configuration.java
+++ src/main/java/com/misterpemodder/shulkerboxtooltip/impl/config/Configuration.java
@@ -16,6 +16,7 @@ import me.sargunvohra.mcmods.autoconfig1u.annotation.Config;
 import me.sargunvohra.mcmods.autoconfig1u.annotation.ConfigEntry;
 import me.sargunvohra.mcmods.autoconfig1u.annotation.ConfigEntry.Gui.EnumHandler.EnumDisplayOption;
 import me.sargunvohra.mcmods.autoconfig1u.gui.registry.GuiRegistry;
+import me.sargunvohra.mcmods.autoconfig1u.shadowed.blue.endless.jankson.Comment;
 import me.sargunvohra.mcmods.autoconfig1u.util.Utils;
 import me.shedaniel.clothconfig2.api.ConfigEntryBuilder;
 import me.shedaniel.clothconfig2.gui.entries.KeyCodeEntry;
@@ -101,41 +102,52 @@ public class Configuration implements ConfigData {
 
   public static class MainCategory {
     @AutoTooltip
+    @Comment("Toggles the shulker box preview")
     public boolean enablePreview = true;
 
     @AutoTooltip
+    @Comment("Locks the preview window above the tooltip.\nWhen locked, the window will not adapt when out of screen.")
     public boolean lockPreview = false;
 
     @AutoTooltip
+    @Comment("Swaps the preview modes.\nIf true, pressing the preview key will show the full preview instead.")
     public boolean swapModes = false;
 
     @AutoTooltip
+    @Comment("If on, the preview is always displayed, regardless of the preview key being pressed.")
     public boolean alwaysOn = false;
 
     @AutoTooltip
+    @Comment("Controls whether the key hints in the container's tooltip should be displayed.")
     public boolean showKeyHints = true;
 
     @AutoTooltip
     @ConfigEntry.Gui.EnumHandler(option = EnumDisplayOption.BUTTON)
+    @Comment("The tooltip to use.\nVANILLA: The vanilla tooltip (shows the first 5 items)\nMOD: The mod's tooltip\nNONE: No tooltip")
     public ShulkerBoxTooltipType tooltipType = ShulkerBoxTooltipType.MOD;
 
     @AutoTooltip
     @ConfigEntry.Gui.EnumHandler(option = EnumDisplayOption.BUTTON)
+    @Comment("In compact mode, how should items with the same ID but different NBT data be compacted?\nIGNORE: Ignores NBT data\nFIRST_ITEM: Items are displayed as all having the same NBT as the first item\nSEPARATE: Separates items with different NBT data")
     public CompactPreviewTagBehavior compactPreviewTagBehavior = CompactPreviewTagBehavior.SEPARATE;
 
     @AutoTooltip
     @ConfigEntry.Gui.EnumHandler(option = EnumDisplayOption.BUTTON)
+    @Comment("Shows info about the current loot table of the item if present.\nVisible only when Tooltip Type is set to Modded.\nHIDE: No loot table info, default.\nSIMPLE: Displays whether the stack uses a loot table.\nADVANCED: Shows the loot table used by the item.")
     public LootTableInfoType lootTableInfoType = LootTableInfoType.HIDE;
 
     @AutoTooltip
+    @Comment("Controls whether the preview window should be colored.")
     public boolean coloredPreview = true;
 
     @AutoTooltip
     @Validator(GreaterThanZero.class)
+    @Comment("The max number of items in a row.\nMay not affect modded containers.")
     public int defaultMaxRowSize = 9;
 
     @AutoTooltip
     @ConfigEntry.Gui.RequiresRestart
+    @Comment("If on, the client will try to send packets to servers to allow extra preview information such as ender chest previews.")
     public boolean serverIntegration = true;
   }
 
@@ -168,9 +180,11 @@ public class Configuration implements ConfigData {
 
   public static class ControlsCategory {
     @AutoTooltip
+    @Comment("Press this key when hovering a container stack to open the preview window.")
     public InputUtil.Key previewKey = InputUtil.Type.KEYSYM.createFromCode(GLFW.GLFW_KEY_LEFT_SHIFT);
 
     @AutoTooltip
+    @Comment("Press this key when hovering a container stack to open the full preview window.")
     public InputUtil.Key fullPreviewKey = InputUtil.Type.KEYSYM.createFromCode(GLFW.GLFW_KEY_LEFT_ALT);
   }
 
@@ -178,11 +192,13 @@ public class Configuration implements ConfigData {
     @AutoTooltip
     @ConfigEntry.Gui.PrefixText
     @ConfigEntry.Gui.RequiresRestart
+    @Comment("If on, the server will be able to provide extra information about containers to the clients with the mod installed.\nDisabling this option will disable all of the options below.")
     public boolean clientIntegration = true;
 
     @AutoTooltip
     @ConfigEntry.Gui.EnumHandler(option = EnumDisplayOption.BUTTON)
     @ConfigEntry.Gui.RequiresRestart
+    @Comment("Changes the way the ender chest content preview is synchronized.\nNONE: No synchronization, prevents clients from seeing a preview of their ender chest.\nACTIVE: Ender chest contents are synchronized when changed.\nPASSIVE: Ender chest contents are synchonized when the client opens a preview.")
     public EnderChestSyncType enderChestSyncType = EnderChestSyncType.ACTIVE;
   }
 
