e43e03af22191faeea835c60957b29a73d888d98
Yanis Guaye <yguaye44@gmail.com>
2020-06-21 13:25:30 +0200

    Fix tooltip in config menu being on one line

--- src/main/java/com/misterpemodder/shulkerboxtooltip/impl/ModMenuCompat.java
+++ src/main/java/com/misterpemodder/shulkerboxtooltip/impl/ModMenuCompat.java
@@ -1,13 +1,33 @@
 package com.misterpemodder.shulkerboxtooltip.impl;
 
 import com.misterpemodder.shulkerboxtooltip.impl.config.Configuration;
+
 import io.github.prospector.modmenu.api.ConfigScreenFactory;
 import io.github.prospector.modmenu.api.ModMenuApi;
 import me.sargunvohra.mcmods.autoconfig1u.AutoConfig;
+import me.sargunvohra.mcmods.autoconfig1u.ConfigManager;
+import me.sargunvohra.mcmods.autoconfig1u.gui.ConfigScreenProvider;
+import me.sargunvohra.mcmods.autoconfig1u.gui.DefaultGuiProviders;
+import me.sargunvohra.mcmods.autoconfig1u.gui.DefaultGuiTransformers;
+import me.sargunvohra.mcmods.autoconfig1u.gui.registry.ComposedGuiRegistryAccess;
+import me.sargunvohra.mcmods.autoconfig1u.gui.registry.DefaultGuiRegistryAccess;
+import me.sargunvohra.mcmods.autoconfig1u.gui.registry.GuiRegistry;
+import me.sargunvohra.mcmods.autoconfig1u.gui.registry.api.GuiRegistryAccess;
 
 public class ModMenuCompat implements ModMenuApi {
+  private static final GuiRegistry defaultGuiRegistry = DefaultGuiTransformers
+      .apply(DefaultGuiProviders.apply(new GuiRegistry()));
+
   @Override
   public ConfigScreenFactory<?> getModConfigScreenFactory() {
-    return screen -> AutoConfig.getConfigScreen(Configuration.class, screen).get();
+    return screen -> new ConfigScreenProvider<Configuration>(
+        (ConfigManager<Configuration>) AutoConfig.getConfigHolder(Configuration.class), getGuiRegistryAccess(), screen)
+            .get();
+  }
+
+  private static GuiRegistryAccess getGuiRegistryAccess() {
+    // We set our gui registry after the dafault one to override the default transformations.
+    return new ComposedGuiRegistryAccess(defaultGuiRegistry, AutoConfig.getGuiRegistry(Configuration.class),
+        new DefaultGuiRegistryAccess());
   }
 }
