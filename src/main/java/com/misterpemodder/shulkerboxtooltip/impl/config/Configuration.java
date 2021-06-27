1f07b5fc8425f908aa69f4b21e13ecc654203a84
Yanis Guaye <yguaye44@gmail.com>
2021-06-27 23:52:17 +0200

    Add default values to the config option comments

--- src/main/java/com/misterpemodder/shulkerboxtooltip/impl/config/Configuration.java
+++ src/main/java/com/misterpemodder/shulkerboxtooltip/impl/config/Configuration.java
@@ -45,21 +45,25 @@ public final class Configuration implements ConfigData {
 
   public static class PreviewCategory implements Cloneable {
     @AutoTooltip
-    @Comment("Toggles the shulker box preview")
+    @Comment("Toggles the shulker box preview.\n"
+        + "(default value: true)")
     public boolean enable = true;
 
     @AutoTooltip
     @Comment("Locks the preview window above the tooltip.\n"
-        + "When locked, the window will not adapt when out of screen.")
+        + "When locked, the window will not adapt when out of screen.\n"
+        + "(default value: false)")
     public boolean lock = false;
 
     @AutoTooltip
     @Comment("Swaps the preview modes.\n"
-        + "If true, pressing the preview key will show the full preview instead.")
+        + "If true, pressing the preview key will show the full preview instead.\n"
+        + "(default value: false)")
     public boolean swapModes = false;
 
     @AutoTooltip
-    @Comment("If on, the preview is always displayed, regardless of the preview key being pressed.")
+    @Comment("If on, the preview is always displayed, regardless of the preview key being pressed.\n"
+        + "(default value: false)")
     public boolean alwaysOn = false;
 
     @AutoTooltip
@@ -67,21 +71,25 @@ public final class Configuration implements ConfigData {
     @Comment("In compact mode, how should items with the same ID but different NBT data be compacted?\n"
         + "IGNORE: Ignores NBT data\n"
         + "FIRST_ITEM: Items are displayed as all having the same NBT as the first item\n"
-        + "SEPARATE: Separates items with different NBT data")
+        + "SEPARATE: Separates items with different NBT data\n"
+        + "(default value: SEPARATE)")
     public CompactPreviewNbtBehavior compactPreviewNbtBehavior = CompactPreviewNbtBehavior.SEPARATE;
 
     @AutoTooltip
-    @Comment("Controls whether the preview window should be colored.")
+    @Comment("Controls whether the preview window should be colored.\n"
+        + "(default value: true)")
     public boolean coloredPreview = true;
 
     @AutoTooltip
     @Validator(GreaterThanZero.class)
-    @Comment("The max number of items in a row.\nMay not affect modded containers.")
+    @Comment("The max number of items in a row.\nMay not affect modded containers.\n"
+        + "(default value: 9)")
     public int defaultMaxRowSize = 9;
 
     @AutoTooltip
     @ConfigEntry.Gui.RequiresRestart
-    @Comment("If on, the client will try to send packets to servers to allow extra preview information such as ender chest previews.")
+    @Comment("If on, the client will try to send packets to servers to allow extra preview information such as ender chest previews.\n"
+        + "(default value: true)")
     public boolean serverIntegration = true;
 
     @AutoTooltip
@@ -90,7 +98,8 @@ public final class Configuration implements ConfigData {
         + "MOD_AUTO: ShulkerBoxTooltip's style using the dark mode setting from LibGui, defaults to light theme if not present.\n"
         + "MOD_LIGHT: ShulkerBoxTooltip's style with vanilla colors.\n"
         + "MOD_DARK: ShulkeBoxTooltip's style with gray preview windows instead of white.\n"
-        + "VANILLA: Mimics the style of vanilla bundle previews.")
+        + "VANILLA: Mimics the style of vanilla bundle previews.\n"
+        + "(default value: MOD_AUTO)")
     public Theme theme = Theme.MOD_AUTO;
 
     @AutoTooltip
@@ -99,11 +108,13 @@ public final class Configuration implements ConfigData {
         + "INSIDE: Inside the item's tooltip.\n"
         + "OUTSIDE: Outside the item's tooltip, moves dependening on the screen borders.\n"
         + "OUTSIDE_TOP: Always at the top of the item's tooltip.\n"
-        + "OUTSIDE_BOTTOM: Always at the bottom of the item's tooltip.")
+        + "OUTSIDE_BOTTOM: Always at the bottom of the item's tooltip.\n"
+        + "(default value: INSIDE)")
     public PreviewPosition position = PreviewPosition.INSIDE;
 
     @AutoTooltip
-    @Comment("If on, large item counts in compact previews will be shortened.")
+    @Comment("If on, large item counts in compact previews will be shortened.\n"
+        + "(default value: true)")
     public boolean shortItemCounts = true;
 
     protected static PreviewCategory copyFrom(PreviewCategory source) {
@@ -144,13 +155,17 @@ public final class Configuration implements ConfigData {
 
   public static class TooltipCategory implements Cloneable {
     @AutoTooltip
-    @Comment("Controls whether the key hints in the container's tooltip should be displayed.")
+    @Comment("Controls whether the key hints in the container's tooltip should be displayed.\n"
+        + "(default value: true)")
     public boolean showKeyHints = true;
 
     @AutoTooltip
     @ConfigEntry.Gui.EnumHandler(option = EnumDisplayOption.BUTTON)
-    @Comment("The tooltip to use.\nVANILLA: The vanilla tooltip (shows the first 5 items)\nMOD: The mod's tooltip\n"
-        + "NONE: No tooltip")
+    @Comment("The tooltip to use.\n"
+        + "VANILLA: The vanilla tooltip (shows the first 5 items)\n"
+        + "MOD: The mod's tooltip\n"
+        + "NONE: No tooltip\n"
+        + "(default value: MOD)")
     public ShulkerBoxTooltipType type = ShulkerBoxTooltipType.MOD;
 
     @AutoTooltip
@@ -159,12 +174,14 @@ public final class Configuration implements ConfigData {
         + "Visible only when Tooltip Type is set to Modded.\n"
         + "HIDE: No loot table info, default.\n"
         + "SIMPLE: Displays whether the stack uses a loot table.\n"
-        + "ADVANCED: Shows the loot table used by the item.")
+        + "ADVANCED: Shows the loot table used by the item.\n"
+        + "(default value: HIDE)")
     public LootTableInfoType lootTableInfoType = LootTableInfoType.HIDE;
 
     @AutoTooltip
     @Comment("If on, the mod hides the custom text on shulker box tooltips.\n"
-        + "Use this option when a server-side preview datapack clashes with the mod.")
+        + "Use this option when a server-side preview datapack clashes with the mod.\n"
+        + "(default value: false)")
     public boolean hideShulkerBoxLore = false;
 
     protected static TooltipCategory copyFrom(TooltipCategory source) {
@@ -197,11 +214,13 @@ public final class Configuration implements ConfigData {
   @Environment(EnvType.CLIENT)
   public static class ControlsCategory implements Cloneable {
     @AutoTooltip
-    @Comment("Press this key when hovering a container stack to open the preview window.")
+    @Comment("Press this key when hovering a container stack to open the preview window.\n"
+        + "(default value: key.keyboard.left.shift)")
     public Key previewKey = Key.defaultPreviewKey();
 
     @AutoTooltip
-    @Comment("Press this key when hovering a container stack to open the full preview window.")
+    @Comment("Press this key when hovering a container stack to open the full preview window.\n"
+        + "(default value: key.keyboard.left.alt)")
     public Key fullPreviewKey = Key.defaultFullPreviewKey();
 
     protected static ControlsCategory copyFrom(ControlsCategory source) {
@@ -220,7 +239,8 @@ public final class Configuration implements ConfigData {
     @ConfigEntry.Gui.PrefixText
     @ConfigEntry.Gui.RequiresRestart
     @Comment("If on, the server will be able to provide extra information about containers to the clients with the mod installed.\n"
-        + "Disabling this option will disable all of the options below.")
+        + "Disabling this option will disable all of the options below.\n"
+        + "(default value: true)")
     public boolean clientIntegration = true;
 
     @AutoTooltip
@@ -229,7 +249,8 @@ public final class Configuration implements ConfigData {
     @Comment("Changes the way the ender chest content preview is synchronized.\n"
         + "NONE: No synchronization, prevents clients from seeing a preview of their ender chest.\n"
         + "ACTIVE: Ender chest contents are synchronized when changed.\n"
-        + "PASSIVE: Ender chest contents are synchonized when the client opens a preview.")
+        + "PASSIVE: Ender chest contents are synchonized when the client opens a preview.\n"
+        + "(default value: ACTIVE)")
     public EnderChestSyncType enderChestSyncType = EnderChestSyncType.ACTIVE;
 
     protected static ServerCategory copyFrom(ServerCategory source) {
