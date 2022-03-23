61b6432acf84423f0a6603d4e77ea68e4421801a
Yanis Guaye <yguaye44@gmail.com>
2022-03-23 05:48:05 +0100

    Always read preview key state from the render thread
    Closes #62
    
    (cherry picked from commit 352a29a16bbfc0d34438bf18aa994b0733bf8a66)

--- src/main/java/com/misterpemodder/shulkerboxtooltip/impl/ShulkerBoxTooltipClient.java
+++ src/main/java/com/misterpemodder/shulkerboxtooltip/impl/ShulkerBoxTooltipClient.java
@@ -1,27 +1,27 @@
 package com.misterpemodder.shulkerboxtooltip.impl;
 
-import java.util.List;
-import java.util.function.Supplier;
-import javax.annotation.Nullable;
 import com.misterpemodder.shulkerboxtooltip.api.PreviewContext;
 import com.misterpemodder.shulkerboxtooltip.api.PreviewType;
 import com.misterpemodder.shulkerboxtooltip.api.ShulkerBoxTooltipApi;
 import com.misterpemodder.shulkerboxtooltip.api.provider.PreviewProvider;
+import com.misterpemodder.shulkerboxtooltip.impl.config.Configuration;
 import com.misterpemodder.shulkerboxtooltip.impl.config.Configuration.ShulkerBoxTooltipType;
 import com.misterpemodder.shulkerboxtooltip.impl.network.ClientNetworking;
+import com.misterpemodder.shulkerboxtooltip.impl.util.Key;
 import io.github.cottonmc.cotton.gui.client.LibGui;
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
 import net.fabricmc.loader.api.FabricLoader;
 import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.util.InputUtil;
 import net.minecraft.item.ItemStack;
-import net.minecraft.text.LiteralText;
-import net.minecraft.text.MutableText;
-import net.minecraft.text.Style;
-import net.minecraft.text.Text;
-import net.minecraft.text.TranslatableText;
+import net.minecraft.text.*;
 import net.minecraft.util.Formatting;
+
+import javax.annotation.Nullable;
+import java.util.List;
+import java.util.function.Supplier;
 
 @Environment(EnvType.CLIENT)
 public final class ShulkerBoxTooltipClient implements ClientModInitializer {
@@ -29,6 +29,9 @@ public final class ShulkerBoxTooltipClient implements ClientModInitializer {
   public static MinecraftClient client;
   private static boolean wasPreviewAccessed = false;
   private static Supplier<Boolean> darkModeSupplier;
+
+  private static boolean previewKeyPressed = false;
+  private static boolean fullPreviewKeyPressed = false;
 
   @Override
   public void onInitializeClient() {
@@ -120,5 +123,31 @@ public final class ShulkerBoxTooltipClient implements ClientModInitializer {
 
   public static boolean isDarkModeEnabled() {
     return darkModeSupplier.get();
+  }
+
+  public static boolean isPreviewKeyPressed() {
+    return previewKeyPressed;
+  }
+
+  public static boolean isFullPreviewKeyPressed() {
+    return fullPreviewKeyPressed;
+  }
+
+  private static boolean isKeyPressed(@Nullable Key key) {
+    if (key == null || key.equals(Key.UNKNOWN_KEY) || key.get().equals(InputUtil.UNKNOWN_KEY))
+      return false;
+    return InputUtil.isKeyPressed(MinecraftClient.getInstance().getWindow().getHandle(), key.get().getCode());
+  }
+
+  public static void updatePreviewKeys() {
+    Configuration config = ShulkerBoxTooltip.config;
+
+    if (config == null) {
+      previewKeyPressed = false;
+      fullPreviewKeyPressed = false;
+    } else {
+      previewKeyPressed = isKeyPressed(config.controls.previewKey);
+      fullPreviewKeyPressed = isKeyPressed(config.controls.fullPreviewKey);
+    }
   }
 }
