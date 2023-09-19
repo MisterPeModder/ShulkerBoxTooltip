efd18a01ffe53fe6f9838f33b67c0fd929a55fa0
Yanis Guaye <yguaye44@gmail.com>
2023-09-19 17:36:37 +0200

    Add missing javadoc tags in API
    (cherry picked from commit d76145a9e5d482e3bc7237521554a3a72df720f0)

--- common/src/main/java/com/misterpemodder/shulkerboxtooltip/api/config/ItemStackMergingStrategy.java
+++ common/src/main/java/com/misterpemodder/shulkerboxtooltip/api/config/ItemStackMergingStrategy.java
@@ -1,7 +1,29 @@
 package com.misterpemodder.shulkerboxtooltip.api.config;
 
+/**
+ * Provides a view over the mod's configuration
+ *
+ * @since 3.3.0
+ */
 public enum ItemStackMergingStrategy {
-  IGNORE, FIRST_ITEM, SEPARATE;
+  /**
+   * Ignore NBT data when merging item stacks.
+   *
+   * @since 3.3.0
+   */
+  IGNORE,
+  /**
+   * Merge regardless of NBT data but use the NBT of the first item stack.
+   *
+   * @since 3.3.0
+   */
+  FIRST_ITEM,
+  /**
+   * Do not merge stacks that differ in NBT data.
+   *
+   * @since 3.3.0
+   */
+  SEPARATE;
 
   @Override
   public String toString() {
