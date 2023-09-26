ed34631a2565b3688231f03dc422f0dc9a7b512c
Yanis Guaye <yguaye44@gmail.com>
2023-09-26 11:40:56 +0200

    Add @ApiStatus.Internal to platform-specific packages
    (cherry picked from commit 12375aa6addad866f4367e85fe8f9dac1a0b33bb)

---
+++ fabric/src/main/java/com/misterpemodder/shulkerboxtooltip/impl/network/fabric/package-info.java
@@ -0,0 +1,4 @@
+@ApiStatus.Internal
+package com.misterpemodder.shulkerboxtooltip.impl.network.fabric;
+
+import org.jetbrains.annotations.ApiStatus;
---
+++ forge/src/main/java/com/misterpemodder/shulkerboxtooltip/impl/forge/package-info.java
@@ -0,0 +1,4 @@
+@ApiStatus.Internal
+package com.misterpemodder.shulkerboxtooltip.impl.forge;
+
+import org.jetbrains.annotations.ApiStatus;
